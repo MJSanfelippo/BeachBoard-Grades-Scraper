d4f10ab328c5f69657dc1b1840686dd03c6a4856
MJSanfelippo <mj.sanfelippo@gmail.com>
2016-11-27 16:24:45 -0800

    Made it way more efficient
    Now it logs in once, grabs the id number of each course and goes
    straight to the grades page for each course. Also implemented PhantomJS
    so that the browser doesn't even open. This reduced the exceution time
    IMMENSELY. It was about 1.5-2 min before. Now it's about 10 seconds.

--- seleniumtester.py
+++ seleniumtester.py
@@ -7,64 +7,60 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
 def init_driver():
-    driver = webdriver.Chrome()
+    driver = webdriver.PhantomJS()
     driver.wait = WebDriverWait(driver, 5)
     return driver
 
-def lookup(driver, query, courseName):
-    driver.get("https://csulb.okta.com/")
+def lookup(driver, username, password, listOfCourses):
+    driver.get("https://bbcsulb.desire2learn.com/")
     try:
         user = driver.wait.until(EC.presence_of_element_located(
-            (By.ID, "input27")))
+            (By.ID, "Username")))
         pw = driver.wait.until(EC.presence_of_element_located(
-            (By.ID, "input34")))
-        user.send_keys("") # YOUR USERNAME HERE
-        pw.send_keys("")   # YOUR PASSWORD HERE
-        button = driver.find_element_by_xpath('//input[@type="submit" and @value="Sign In"]')
+            (By.ID, "Password")))
+        user.send_keys(username) # YOUR USERNAME HERE
+        pw.send_keys(password)   # YOUR PASSWORD HERE
+        button = driver.find_element_by_xpath('//input[@type="submit" and @value="Login"]')
         button.click()
-        time.sleep(5)
-        window_before = driver.window_handles[0]
-        bb = driver.find_element_by_xpath('//img[@src="https://csulb.okta.com/bc/image/fileStoreRecord?id=fs0r9zeso8JAELNNYTUJ"]')
-        bb.click()
-        time.sleep(10)
-        window_after = driver.window_handles[1]
-        driver.switch_to_window(window_after)
-        course = driver.wait.until(EC.presence_of_element_located((
-            By.LINK_TEXT, courseName)))
-        course.click()
-        grades = driver.wait.until(EC.presence_of_element_located((
-            By.LINK_TEXT, "Grades")))
-        grades.click()
-        html = driver.page_source
-        soup = BeautifulSoup(html,'html.parser')
-        myPoints= []
-        totalPoints = []
-        for tag in soup.find_all('td'):
-            x = tag.text
-            if not x.endswith('%'):
-                data = x.split("/")
-                try:
-                    if not float(data[0]) == 0.0:
-                        myPoints.append(float(data[0]))
-                        totalPoints.append(float(data[1]))
-                except Exception:
-                    print(end='')
-        sumMine = sum(myPoints)
-        sumTotal = sum(totalPoints)
-        grade = (sumMine/sumTotal)*100
-        msg = "Course name: " + courseName + "\nGrade: " + str(round(grade,2))+"%"
-        print(msg)
+        coursesGradesURLS = []
+        for i in range(len(listOfCourses)):
+            course = driver.wait.until(EC.presence_of_element_located((
+                By.LINK_TEXT, listOfCourses[i])))
+            x = course.get_attribute("href")
+            x = x[(len(x)) - 6:]
+            coursesGradesURLS.append("https://bbcsulb.desire2learn.com/d2l/lms/grades/my_grades/main.d2l?ou="+x)
+        for i in range(len(coursesGradesURLS)):
+            driver.get(coursesGradesURLS[i])
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            myPoints = []
+            totalPoints = []
+            for tag in soup.find_all('td'):
+                x = tag.text
+                if not x.endswith('%'):
+                    data = x.split("/")
+                    try:
+                        if not float(data[0]) == 0.0:
+                            myPoints.append(float(data[0]))
+                            totalPoints.append(float(data[1]))
+                    except Exception:
+                        print(end='')
+            sumMine = sum(myPoints)
+            sumTotal = sum(totalPoints)
+            grade = (sumMine / sumTotal) * 100
+            msg = "Course name: " + courses[i] + "\nGrade: " + str(round(grade, 2)) + "%"
+            print(msg)
+        driver.quit()
     except TimeoutException:
         print("Box or Button not found in the site")
 
 if __name__ == "__main__":
     driver = init_driver()
-    lookup(driver, "Selenium","REC 141 Sec 04 10919 Intro to Leisure Services")
-    driver = init_driver()
-    lookup(driver, "Selenium", "BIOL 200 Sec 01A 1147 General Biology")
-    driver = init_driver()
-    lookup(driver, "Selenium", "CECS 229 Sec 03 10376 Discrete Struct Comp Applic II")
-    driver = init_driver()
-    lookup(driver, "Selenium", "CECS 341 Sec 05 11273 Computer Architect Organizatin")
-    driver = init_driver()
-    lookup(driver, "Selenium", "CECS 343 Sec 01 4057 Intro to Software Engineering")
+    courses = ["REC 141 Sec 04 10919 Intro to Leisure Services",
+               "BIOL 200 Sec 01A 1147 General Biology",
+               "CECS 229 Sec 03 10376 Discrete Struct Comp Applic II",
+               "CECS 341 Sec 05 11273 Computer Architect Organizatin",
+               "CECS 343 Sec 01 4057 Intro to Software Engineering"]
+    courses.sort()
+    # lookup(driver, [USERNAME HERE], [PASSWORD HERE], courses)
+    lookup(driver, "", "", courses)
