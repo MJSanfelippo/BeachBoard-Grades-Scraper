3047be1fd39bc7217e09a40bd61948fd650d3dd1
MJSanfelippo <mj.sanfelippo@gmail.com>
2016-11-27 23:32:05 -0800

    Added comments
    Added comments explaining what everything does just in case I forget

--- seleniumtester.py
+++ seleniumtester.py
@@ -5,62 +5,65 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
 
+#initiate the driver with either Chrome or PhantomJS
 def init_driver():
-    driver = webdriver.PhantomJS()
+    driver = webdriver.Chrome()
     driver.wait = WebDriverWait(driver, 5)
     return driver
 
 def lookup(driver, username, password, listOfCourses):
-    driver.get("https://bbcsulb.desire2learn.com/")
+    driver.get("https://bbcsulb.desire2learn.com/") #Send it to beachboard
     try:
         user = driver.wait.until(EC.presence_of_element_located(
-            (By.ID, "Username")))
+            (By.ID, "Username"))) # finds the username box
         pw = driver.wait.until(EC.presence_of_element_located(
-            (By.ID, "Password")))
-        user.send_keys(username) # YOUR USERNAME HERE
-        pw.send_keys(password)   # YOUR PASSWORD HERE
-        button = driver.find_element_by_xpath('//input[@type="submit" and @value="Login"]')
-        button.click()
-        coursesGradesURLS = []
+            (By.ID, "Password"))) # finds the password box
+        user.send_keys(username) # types the username into the box
+        pw.send_keys(password)   # types the password into the box
+        pw.send_keys(Keys.RETURN) # presses enter while in the password box - this logs in for you
+        coursesGradesURLS = [] # list to hold the grades urls for the courses
         for i in range(len(listOfCourses)):
             course = driver.wait.until(EC.presence_of_element_located((
-                By.LINK_TEXT, listOfCourses[i])))
-            x = course.get_attribute("href")
-            x = x[(len(x)) - 6:]
-            coursesGradesURLS.append("https://bbcsulb.desire2learn.com/d2l/lms/grades/my_grades/main.d2l?ou="+x)
+                By.LINK_TEXT, listOfCourses[i]))) # finds the link of the course
+            courseID = course.get_attribute("href") # finds the href attribute (the place that it goes when clicked)
+            courseID = courseID[(len(courseID)) - 6:] # this strips the href into just the last 6 elements in it which is the course's ID
+            coursesGradesURLS.append("https://bbcsulb.desire2learn.com/d2l/lms/grades/my_grades/main.d2l?ou="+courseID) # this url is the same for all course grade pages except for the id
         for i in range(len(coursesGradesURLS)):
-            driver.get(coursesGradesURLS[i])
-            html = driver.page_source
-            soup = BeautifulSoup(html, 'html.parser')
+            driver.get(coursesGradesURLS[i]) # sends the driver to the grades page
+            html = driver.page_source # get the html of the page
+            soup = BeautifulSoup(html, 'html.parser') # initiate soup
             myPoints = []
             totalPoints = []
-            for tag in soup.find_all('td'):
-                x = tag.text
-                if not x.endswith('%'):
-                    data = x.split("/")
+            for tdTag in soup.find_all('td'): # finds all td tags
+                textOfTd = tdTag.text #gets the text of the td tag
+                if not textOfTd.endswith('%'):  # continue if the text doesnt end with a %
+                    data = textOfTd.split("/") # split it by the division sign, effectively getting yourPoints/totalPoints
                     try:
-                        if not float(data[0]) == 0.0:
-                            myPoints.append(float(data[0]))
-                            totalPoints.append(float(data[1]))
-                    except Exception:
+                        if not float(data[0]) == 0.0: # this assumes that if you got a 0 on an assignment, it's because the prof just hasn't entered the grade - so this
+                                                      # script won't work if you legitimately did get a 0 on an assignment
+                            myPoints.append(float(data[0])) # gets your points
+                            totalPoints.append(float(data[1])) # gets that assignment's total points
+                    except Exception: # This essentially is a failsafe
                         print(end='')
-            sumMine = sum(myPoints)
-            sumTotal = sum(totalPoints)
-            grade = (sumMine / sumTotal) * 100
-            msg = "Course name: " + courses[i] + "\nGrade: " + str(round(grade, 2)) + "%"
+            sumMine = sum(myPoints) # gets the sum of all of your points
+            sumTotal = sum(totalPoints) # gets the sum of all of the total points available
+            grade = (sumMine / sumTotal) * 100 # determines your grade
+            msg = "Course name: " + courses[i] + "\nGrade: " + str(round(grade, 2)) + "%" # prints your grade rounded to 2 decimal places
             print(msg)
         driver.quit()
     except TimeoutException:
-        print("Box or Button not found in the site")
+        print("Element not found in the site in time")
 
 if __name__ == "__main__":
     driver = init_driver()
+    # list of courses
     courses = ["REC 141 Sec 04 10919 Intro to Leisure Services",
                "BIOL 200 Sec 01A 1147 General Biology",
+               "BIOL 200 Sec 11 1154 General Biology",
                "CECS 229 Sec 03 10376 Discrete Struct Comp Applic II",
                "CECS 341 Sec 05 11273 Computer Architect Organizatin",
                "CECS 343 Sec 01 4057 Intro to Software Engineering"]
-    courses.sort()
     # lookup(driver, [USERNAME HERE], [PASSWORD HERE], courses)
     lookup(driver, "", "", courses)
